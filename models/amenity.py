e48ab03dbdf2c6cef4cf4ebac06740d5668d09c0
KFredlund <katihomes@gmail.com>
2020-02-04 19:35:27 +0000

    [New] Create a class Amenity that inherits from BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Class Amenity that inherits from BaseModel """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Class Amenity
+    Public Class Attribute:
+    name: string - empty string
+    """
+
+    def __init__(self):
+        """ Constructor for Public Class Attribute """
+        self.name = name
