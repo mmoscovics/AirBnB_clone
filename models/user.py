63202bfbb1219cfc269253fb7b7aca77474ce9c3
KFredlund <katihomes@gmail.com>
2020-02-04 19:20:54 +0000

    [New] Create a class User that inherits from BaseModel

---
+++ models/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Class User that inherits from BaseModel """
+
+
+class User(BaseModel):
+    """Class User
+    Public Instance Attributes:
+    email: string - empty string
+    password: string - empty string
+    first_name: string - empty string
+    last_name: string - empty string
+    """
+
+    def __init__(self):
+        """ Constructor for Public Instance Attributes """
+
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
