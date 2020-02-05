f2c3f133ff1261358cc8a299f49f01fd11820d43
KFredlund <katihomes@gmail.com>
2020-02-05 06:09:43 +0000

    [Update] Add tests for attributes

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,7 +5,8 @@ import unittest
 from models.base_model import BaseModel
 from models.user import User
 from tests.test_models.test_base_model import TestBaseModel
-
+from uuid import UUID
+from datetime import datetime as dt
 
 class TestUser(TestBaseModel):
     """ User Tests """
@@ -16,3 +17,11 @@ class TestUser(TestBaseModel):
         super()__init__(*args, **kwargs)
         self._class = User
         self._name = "User"
+
+    def test_attrs(self):
+    """ Test for attributes """
+
+        self.assertIsInstance(self._class.email, str)
+        self.assertIsInstance(self._class.password, str)
+        self.assertIsInstance(self._class.first_name, str)
+        self.assertIsInstance(self._class.last_name, str)
