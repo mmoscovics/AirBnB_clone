e4e34594b08b41c2661c1eb8f2324eebc85add92
Mitchell <mmoscovics@yahoo.com>
2020-02-16 22:05:53 +0000

    [Create] base state tests

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Tests for class State """
+
+import unittest
+from models.base_model import BaseModel
+from models.state import State
+from tests.test_models.test_base_model import TestBaseModel
+
+
+class TestState(TestBaseModel):
+    """ State Tests """
+
+    def __init__(self, *args, **kwargs):
+        """ Constructor attributes and objects to test """
+
+        super()__init__(*args, **kwargs)
+        self._class = State
+        self._name = "State"
