a023e3f3deba6f7a376fe7f9885f6eb4f9c7d336
Mitchell <mmoscovics@yahoo.com>
2020-02-16 22:05:24 +0000

    [Create] base place tests

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Tests for class Place """
+
+import unittest
+from models.base_model import BaseModel
+from models.place import Place
+from tests.test_models.test_base_model import TestBaseModel
+
+
+class TestPlace(TestBaseModel):
+    """ Place Tests """
+
+    def __init__(self, *args, **kwargs):
+        """ Constructor attributes and objects to test """
+
+        super()__init__(*args, **kwargs)
+        self._class = Place
+        self._name = "Place"
