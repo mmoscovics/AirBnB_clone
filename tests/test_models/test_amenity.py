8e136d7bacdb71d86b4541e1989ec564b3089264
Mitchell <mmoscovics@yahoo.com>
2020-02-16 22:04:57 +0000

    [Create] base amenity tests

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Tests for class Amenity """
+
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from tests.test_models.test_base_model import TestBaseModel
+
+
+class TestAmenity(TestBaseModel):
+    """ Amenity Tests """
+
+    def __init__(self, *args, **kwargs):
+        """ Constructor attributes and objects to test """
+
+        super()__init__(*args, **kwargs)
+        self._class = Amenity
+        self._name = "Amenity"
