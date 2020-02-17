eec6fe0e1f040e2e0f9dd93bf944af14ac111844
Mitchell <mmoscovics@yahoo.com>
2020-02-17 21:27:59 +0000

    [Create] base for filestorage tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Tests for class FileStorage """
+
+import unittest
+from models.engine.file_storage import FileStorage
+import os
+import json
+
+class TestFileStorage(unittest.TestCase):
+    """ FileStorage Tests """
+
+    def test_attributes(self):
+        """ Tests class attributes """
+
+        pass
